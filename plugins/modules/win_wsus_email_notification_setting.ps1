3aeb47167ecc5054eef37c8a11f069e9e2a11fac
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-01-19 17:47:08 +0100

    feat: Add smtp password

--- plugins/modules/win_wsus_email_notification_setting.ps1
+++ plugins/modules/win_wsus_email_notification_setting.ps1
@@ -13,6 +13,7 @@ $spec = @{
         smtp_port                      = @{ type = "int"; default = 25 }
         smtp_username                  = @{ type = "str" }
         smtp_password                  = @{ type = "str" }
+        smtp_password_update           = @{ type = "str"; choices = "always", "on_create"; default = "on_create" }
         smtp_authentication_required   = @{ type = "bool"; default = $false }
         sender_display_name            = @{ type = "str" }
         sender_email_address           = @{ type = "str" }
@@ -20,8 +21,8 @@ $spec = @{
         send_sync_notification         = @{ type = "bool"; default = $false }
         sync_notification_recipients   = @{ type = "list"; elements = "str" }
         send_status_notification       = @{ type = "bool"; default = $false }
-        status_notification_frequency   = @{ type = "str"; choices = "daily", "weekly"; default = "daily" }
-        status_notification_time        = @{ type = "str" }
+        status_notification_frequency  = @{ type = "str"; choices = "daily", "weekly"; default = "daily" }
+        status_notification_time       = @{ type = "str" }
         status_notification_recipients = @{ type = "list"; elements = "str" }
         state                          = @{ type = "str"; choices = "absent", "present"; default = "present" }
     }
@@ -40,6 +41,7 @@ $smtpHost = $module.Params.smtp_host
 $smtpPort = $module.Params.smtp_port
 $smtpUsername = $module.Params.smtp_username
 $smtpPassword = $module.Params.smtp_password
+$smtpPasswordUpdate = $module.Params.smtp_password_update
 $smtpAuthenticationRequired = $module.Params.smtp_authentication_required
 $senderDisplayName = $module.Params.sender_display_name
 $senderEmailAddress = $module.Params.sender_email_address
@@ -73,58 +75,82 @@ catch {
 # ------------------
 
 # SMTP Host
-if (($wsusConfig.SmtpHostName) -and $smtpHost -and ($state -eq "absent")) {
+if ($smtpHost -and ($wsusConfig.SmtpHostName) -and ($state -eq "absent")) {
     try {
         $wsusConfig.SmtpHostName = ""
-        $wsusConfig.save()
+        $wsusConfig.Save()
         $module.Result.changed = $true
     }
     catch {
         $module.FailJson("Failed to remove smtp host", $Error[0])
     }
 }
-elseif (($wsusConfig.SmtpHostName -ne $smtpHost) -and ($state -eq "present")) {
+elseif ($smtpUsername -and ($wsusConfig.SmtpHostName -ne $smtpHost) -and ($state -eq "present")) {
     try {
         $wsusConfig.SmtpHostName = $smtpHost
-        $wsusConfig.save()
+        $wsusConfig.Save()
         $module.Result.changed = $true
     }
     catch {
-        $module.FailJson("Failed to set new smtp host", $Error[0])
+        $module.FailJson("Failed to set smtp host", $Error[0])
     }
 }
 
 # SMTP Port
-if (($wsusConfig.SmtpPort -ne $smtpPort) -and ($state -eq "present")) {
+if ($smtpPort -and ($wsusConfig.SmtpPort -ne $smtpPort) -and ($state -eq "present")) {
     try {
         $wsusConfig.SmtpPort = $smtpPort
-        $wsusConfig.save()
+        $wsusConfig.Save()
         $module.Result.changed = $true
     }
     catch {
-        $module.FailJson("Failed to set new smtp port", $Error[0])
+        $module.FailJson("Failed to set smtp port", $Error[0])
     }
 }
 
 # SMTP Username
-if (($wsusConfig.SmtpUserName) -and $smtpUsername -and ($state -eq "absent")) {
+if ($smtpUsername -and ($wsusConfig.SmtpUserName) -and ($state -eq "absent")) {
     try {
         $wsusConfig.SmtpUserName = ""
-        $wsusConfig.save()
+        $wsusConfig.Save()
         $module.Result.changed = $true
     }
     catch {
         $module.FailJson("Failed to remove smtp username", $Error[0])
     }
 }
-elseif (($wsusConfig.SmtpUserName -ne $smtpUsername) -and ($state -eq "present")) {
+elseif ($smtpUsername -and ($wsusConfig.SmtpUserName -ne $smtpUsername) -and ($state -eq "present")) {
     try {
         $wsusConfig.SmtpUserName = $smtpUsername
         $wsusConfig.save()
         $module.Result.changed = $true
     }
     catch {
-        $module.FailJson("Failed to set new smtp host", $Error[0])
+        $module.FailJson("Failed to set smtp host", $Error[0])
+    }
+}
+
+# SMTP Password
+if ($smtpPassword -and ($wsusConfig.HasSmtpUserPassword) -and ($state -eq "absent")) {
+    try {
+        $wsusConfig.SetSmtpUserPassword("")
+        $module.Result.changed = $true
+    }
+    catch {
+        $module.FailJson("Failed to remove smtp password", $Error[0])
+    }
+}
+elseif (
+    $smtpPassword -and
+    (((-not $wsusConfig.HasSmtpUserPassword) -and ($state -eq "present")) -or
+    (($wsusConfig.HasSmtpUserPassword) -and ($smtpPasswordUpdate -eq "always") -and ($state -eq "present")))
+) {
+    try {
+        $wsusConfig.SetSmtpUserPassword($smtpPassword)
+        $module.Result.changed = $true
+    }
+    catch {
+        $module.FailJson("Failed to set smtp password", $Error[0])
     }
 }
 
