f50bb3305f622d388f73f65be63bbdceabb44010
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-01-23 18:43:23 +0100

    fix: Parameters

--- plugins/modules/win_wsus_install_approval_rule.ps1
+++ plugins/modules/win_wsus_install_approval_rule.ps1
@@ -9,9 +9,9 @@
 $spec = @{
     options             = @{
         name                  = @{ type = "str"; required = $true }
-        computer_target_group = @{ type = "list"; element = "str" }
-        update_classification = @{ type = "list"; element = "str" }
-        update_product        = @{ type = "list"; element = "str" }
+        computer_target_group = @{ type = "list"; elements = "str" }
+        update_classification = @{ type = "list"; elements = "str" }
+        update_product        = @{ type = "list"; elements = "str" }
         deadline              = @{ type = "str" }
         state                 = @{ type = "str"; choices = "absent", "present"; default = "present" }
     }
